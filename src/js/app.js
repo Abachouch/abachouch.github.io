1711134d98146f61e1d2169949b251408bdde664
youssouf <youssouf.abachouch@gmail.com>
2018-07-31 11:24:21 +0100

    init version 2

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+// CSS
+require('../sass/main.scss');
+// JS
+
+
+window.$ = require('jquery');
+import $ from 'jquery'
+
+window.inView = require('in-view'); 
+
+import '../../node_modules/bootstrap/js/dist/collapse';
+
+require('./main.js')
